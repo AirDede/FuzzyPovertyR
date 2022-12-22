23af260adcd27806fa0f6fd49fc4b7647db3656c
AirDede <federico.crescenzi@unitus.it>
2022-12-22 12:09:50 +0100

    Added dataset
    added dataset (artificial) with incomplete/sketched description

---
+++ R/eusilc-data.R
@@ -0,0 +1,17 @@
+#' Eusilc data
+#'
+#' @docType data
+#'
+#' @usage data(grav)
+#'
+#' @format An object of class \code{"data.frame"}
+#'
+#' @keywords datasets
+#'
+#' @references Moore et al. (2013) Genetics 195:1077-1086
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#'
+#' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
+#'
+#' @examples
+#' data(eusilc)
