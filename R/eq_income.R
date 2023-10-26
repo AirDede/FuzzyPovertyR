d51fa2a16d71b6cc38e1330d358a75587d558c3b
AirDede <federico.crescenzi@unitus.it>
2023-10-26 16:06:21 +0200

    Update eq_income.R
    eq_income is now eq_predicate

--- R/eq_income.R
+++ R/eq_income.R
@@ -19,40 +19,40 @@
 #' aa=runif(100, 0, 1000) # predicate
 #' ncomp=rep(c(1,3,5,7,4),20) #n componenti
 #' age16=ncomp-1 #componenti < 16
-#' eq_income(predicate=aa, ncomp=ncomp, scale.eq="carbonaro") #carbonaro
-#' eq_income(predicate=aa, ncomp=ncomp, scale.eq="n.par") #non-parametric
-#' eq_income(predicate=aa, ncomp=ncomp, age=age16, scale.eq="OECD7050") #OECD7050
-#' eq_income(predicate=aa, ncomp=ncomp, age=age16, scale.eq="modifiedOECD") #modifiedOECD
+#' eq_predicate(predicate=aa, ncomp=ncomp, scale.eq="carbonaro") #carbonaro
+#' eq_predicate(predicate=aa, ncomp=ncomp, scale.eq="n.par") #non-parametric
+#' eq_predicate(predicate=aa, ncomp=ncomp, age=age16, scale.eq="OECD7050") #OECD7050
+#' eq_predicate(predicate=aa, ncomp=ncomp, age=age16, scale.eq="modifiedOECD") #modifiedOECD
 #' newscal=data.frame("ncomp"=c(1:9), "s.eq"=runif(9,1,10)) # new
 #' ncomp=rep(c(10,3,5,7,4),20)
-#' eq_income(predicate=aa, ncomp=ncomp,scale.eq="new", newscale=newscal)
+#' eq_predicate(predicate=aa, ncomp=ncomp,scale.eq="new", newscale=newscal)
 #'
 
-eq_income = function(predicate, ncomp, age, scale.eq,newscale){
+eq_predicate = function(predicate, ncomp, age, scale.eq,newscale){
   if (scale.eq=="carbonaro"){
     datascale=data.frame("ncomp"=c(1:7), "s.eq"=c(1,1.67,2.23,2.73,3.18,3.61,4.01))
     ncomp[ncomp>7]=7
-    eq_income=data.frame("predicate"=predicate, "ncomp"=ncomp)
-    eq_income=dplyr::inner_join(eq_income, datascale, by="ncomp")
-    eq_income$predicate.eq=eq_income$predicate/eq_income$s.eq
+    eq_predicate=data.frame("predicate"=predicate, "ncomp"=ncomp)
+    eq_predicate=dplyr::inner_join(eq_predicate, datascale, by="ncomp")
+    eq_predicate$predicate.eq=eq_predicate$predicate/eq_predicate$s.eq
   } else if (scale.eq=="n.par"){
     datascale=data.frame("ncomp"=c(1:7), "s.eq"=c(1,1.69,2.08,2.41,2.92,3.32, 3.32))
-    eq_income=data.frame("predicate"=predicate, "ncomp"=ncomp)
+    eq_predicate=data.frame("predicate"=predicate, "ncomp"=ncomp)
     ncomp[ncomp>7]=7
-    eq_income=dplyr::inner_join(eq_income, datascale, by="ncomp")
-    eq_income$predicate.eq=eq_income$predicate/eq_income$s.eq
+    eq_predicate=dplyr::inner_join(eq_predicate, datascale, by="ncomp")
+    eq_predicate$predicate.eq=eq_predicate$predicate/eq_predicate$s.eq
   } else if (scale.eq=="OECD7050"){
-    eq_income=data.frame("predicate"=predicate, "ncomp"=ncomp, "age"=age, "s.eq"=(1+(ncomp-1-age)*0.7+age*0.5))
-    eq_income$predicate.eq= eq_income$predicate/ eq_income$s.eq
+    eq_predicate=data.frame("predicate"=predicate, "ncomp"=ncomp, "age"=age, "s.eq"=(1+(ncomp-1-age)*0.7+age*0.5))
+    eq_predicate$predicate.eq= eq_predicate$predicate/ eq_predicate$s.eq
   } else if (scale.eq=="modifiedOECD"){
-    eq_income=data.frame(  "predicate"=predicate, "ncomp"=ncomp, "age"=age,"s.eq"=(1+(ncomp-1-age)*0.5+age*0.3))
-    eq_income$predicate.eq= eq_income$predicate/ eq_income$s.eq
+    eq_predicate=data.frame(  "predicate"=predicate, "ncomp"=ncomp, "age"=age,"s.eq"=(1+(ncomp-1-age)*0.5+age*0.3))
+    eq_predicate$predicate.eq= eq_predicate$predicate/ eq_predicate$s.eq
   } else if (scale.eq=="new") {
     ncomp[ncomp>max(newscale$ncomp)]=max(newscale$ncomp)
     datascale=newscale
-    eq_income=data.frame("predicate"=predicate, "ncomp"=ncomp)
-    eq_income=dplyr::inner_join(eq_income, datascale, by="ncomp")
-    eq_income$predicate.eq=eq_income$predicate/eq_income$s.eq
+    eq_predicate=data.frame("predicate"=predicate, "ncomp"=ncomp)
+    eq_predicate=dplyr::inner_join(eq_predicate, datascale, by="ncomp")
+    eq_predicate$predicate.eq=eq_predicate$predicate/eq_predicate$s.eq
   }
-  return(eq_income)
+  return(eq_predicate)
 }
